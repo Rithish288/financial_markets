020dbd36476f713b580d186cc0e6ed2c453b12f8
Rithish Kondi Ramesh <rithish28.program.music@gmail.com>
2026-06-05 01:46:37 +0100

    declarations for adding an order

--- order_book.cpp
+++ order_book.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "order_book.h"
+
+uint64_t OrderBook::add_order(const double price, const uint32_t quantity, const bool is_bid) {
+    if (is_bid) {
+        return add_bid(price, quantity);
+    }
+   return add_ask(price, quantity);
+}
+
+uint64_t OrderBook::add_bid(const double price, const uint32_t quantity) {
+    bid_list.emplace_back(price, quantity);
+    return bid_list.back().order_id;
+}
+
+uint64_t OrderBook::add_ask(const double price, const uint32_t quantity) {
+    ask_list.emplace_back(price, quantity);
+    return ask_list.back().order_id;
+}
+
+OrderBook::~OrderBook() {
+    bid_list.clear();
+    ask_list.clear();
+}
--- order_book.h
+++ order_book.h
@@ -4,16 +4,46 @@
 
 #ifndef FINANCIAL_MARKETS_ORDER_BOOK_H
 #define FINANCIAL_MARKETS_ORDER_BOOK_H
+
 #include <chrono>
+#include <list>
 
-
-class order_book {
+struct Order {
+    double price;
+    uint32_t quantity;
+    uint64_t timestamp;
+    uint64_t order_id;
+    inline static uint64_t order_id_counter = 0;
+    Order(const double price, const uint32_t quantity) :
+    price(price),
+    quantity(quantity),
+    order_id(order_id_counter++) {
+        const auto now = std::chrono::high_resolution_clock::now();
+        timestamp = std::chrono::duration_cast<std::chrono::nanoseconds>(now.time_since_epoch()).count();
+    };
 };
 
-class order {
-    double price;
-    int quantity;
-    std::chrono::time_point<std::chrono::system_clock> timestamp;
+class OrderBook {
+public:
+    std::vector<Order> bid_list;
+    std::vector<Order> ask_list;
+
+    /**
+     * Creates and adds a new order to the order queue
+     * @param price the price (per share) of the bid/ask
+     * @param quantity the number of shares to buy
+     * @param is_bid 1=bid and 0=ask
+     * @return the id of the order
+     */
+    uint64_t add_order(double price, uint32_t quantity, bool is_bid);
+    void remove_order(uint64_t order_id);
+    void match_orders();
+
+    ~OrderBook();
+
+private:
+    uint64_t add_bid(double price, uint32_t quantity);
+    uint64_t add_ask(double price, uint32_t quantity);
 };
 
 #endif //FINANCIAL_MARKETS_ORDER_BOOK_H
